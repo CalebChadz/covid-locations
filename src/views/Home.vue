76922d4805d85ad98df87157e00387bc7a163c97
CalebChadz <calebchadz@gmail.com>
2021-10-31 14:46:16 +1300

    initialised base application

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,36 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <l-map :center="[47.41322, -1.219482]">
+       <l-tile-layer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      ></l-tile-layer>
+      <l-control-layers />
+    </l-map>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import {
+  LMap,  
+  LTileLayer,
+  LMarker,
+  LControlLayers,
+} from "@vue-leaflet/vue-leaflet";
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld,
+    LMap,
+    LTileLayer,
+    LMarker,
+    LControlLayers,
   },
 };
 </script>
+<style lang="scss">
+.home {
+  width: 100%;
+  height: 100%;
+}
+</style>
