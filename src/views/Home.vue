01f3e9cac4ae08144fddfb528766530615c8013c
CalebChadz <calebchadz@gmail.com>
2021-11-05 10:30:40 +1300

    Requests GeoJson  and prepares it for loading on map.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,30 +1,52 @@
 <template>
-  <div class="home">
-    <l-map :center="[47.41322, -1.219482]">
-       <l-tile-layer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      ></l-tile-layer>
-      <l-control-layers />
-    </l-map>
-  </div>
+<div class="home">
+  <l-map style="height:50vh">
+    <l-geo-json :geojson="geojson" />
+  </l-map>
+</div>
 </template>
 
 <script>
-// @ is an alias to /src
+import "leaflet/dist/leaflet.css";
 import {
-  LMap,  
-  LTileLayer,
-  LMarker,
-  LControlLayers,
+LMap, LGeoJson
 } from "@vue-leaflet/vue-leaflet";
 
 export default {
   name: 'Home',
   components: {
     LMap,
-    LTileLayer,
-    LMarker,
-    LControlLayers,
+    LGeoJson,
+  },
+  setup(){
+    //request the geoJSOn from ministry website "https://raw.githubusercontent.com/minhealthnz/nz-covid-data/main/locations-of-interest/august-2021/locations-of-interest.geojson"
+    const LOIURL = 'https://raw.githubusercontent.com/minhealthnz/nz-covid-data/main/locations-of-interest/august-2021/locations-of-interest.geojson';
+    async function getProviders() {
+      //request the geojson store into response.
+      const response = await fetch(LOIURL);
+      const geoJsonText = await response.text();
+      const geojson = {
+        geojson: {
+
+        },
+        geojsonOptions: {
+
+        },
+      };
+      geojson.geojson = JSON.parse(geoJsonText);
+      console.log(geojson);
+      return geojson;
+    }
+    return getProviders(); 
+  },
+  async beforeMount() {
+  // HERE is where to load Leaflet components!
+  const { circleMarker } = await import("leaflet/dist/leaflet-src.esm");
+
+  // And now the Leaflet circleMarker function can be used by the options:
+  this.geojsonOptions.pointToLayer = (feature, latLng) =>
+    circleMarker(latLng, { radius: 8 });
+  this.mapIsReady = true;
   },
 };
 </script>
